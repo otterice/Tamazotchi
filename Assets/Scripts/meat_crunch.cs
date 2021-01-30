241efaf1670286d819740b24923bb1196389d601
Dankahdo <dakdo2001@gmail.com>
2021-01-30 14:55:20 -0800

    Revert "better gravity feel and petting feature"
    This reverts commit 5994db3ee7b0ac938e8397d510ed7716db1fd6ca.

--- Assets/Scripts/meat_crunch.cs
+++ Assets/Scripts/meat_crunch.cs
@@ -9,7 +9,7 @@ public class meat_crunch : MonoBehaviour
 
         Debug.Log("meat eat");
 
-        if (other.gameObject.name == "Petr") {
+        if (other.gameObject.name == "Petr hitbox") {
             GameObject m = Instantiate(meat_splatter) as GameObject;
             m.transform.position = transform.position;
 
