9b058c493157977ccb17a1138d0e44ab03819159
Jonathan Thai <36181811+jonathanht@users.noreply.github.com>
2021-01-30 12:12:55 -0800

    unity user gps feature

---
+++ Assets/Scripts/GPS.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPS : MonoBehaviour
+{
+    public static GPS Instance { set; get; }
+    public float latitude;
+    public float longitude;
+
+
+    void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());      
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if(!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GSP");
+            yield break;
+        }
+
+        Input.location.Start();
+        //Timeout 
+        int maxWait = 20;
+
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if(maxWait <= 0)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+
+        yield break;
+    }
+}
---
+++ Assets/Scripts/UpdateGPS.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpdateGPS : MonoBehaviour
+{
+
+    public Text coordinates;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        coordinates.text = "Lat: " + GPS.Instance.latitude.ToString() + "  Lon: " + GPS.Instance.longitude.ToString();
+    }
+}
