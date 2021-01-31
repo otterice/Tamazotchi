e164731c94192da62a6784e47ed87ff0e22b5648
Jonathan Thai <36181811+jonathanht@users.noreply.github.com>
2021-01-30 16:52:26 -0800

    Pedometer tuning :wink:

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
     public Text steps;
-    int stepCount = 0;
+    double stepCount = 0;
     private void Start()
     {
         Accelerometer.Instance.OnShake += WhenShake;
@@ -21,8 +22,9 @@ public class Counter : MonoBehaviour
     private void WhenShake()
     {
        
-        stepCount += 1;
-        steps.text = "Steps: " + stepCount;
+        stepCount += 0.052;
+        int roundCount = Convert.ToInt32(stepCount);
+        steps.text = "Steps: " + roundCount;
         Debug.Log(stepCount);
     }
 
